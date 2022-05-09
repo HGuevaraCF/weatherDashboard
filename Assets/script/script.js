41f0206f417bcc83f9c9b42121adda5c16054608
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-05-08 22:29:52 -0500

    general weather complete

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,45 +1,38 @@
-const button = $('#submitBtn');
-const country = $('#CountryListSelect').val();
+// const moment = require("moment");
+
 const APIKey = 'c0e8f455257235f308e6f6b97f8d9c8e';
-// console.log(moment().format('MMMM Do YYYY'));
 
-button.on('click', function(event) {
+$('#submitBtn').on('click', function (event) {
+    var country = $('#CountryListSelect').val();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${APIKey}&units=metric`).then(response => response.text())
+        .then(result => {
+            console.log(result);
+            var data = JSON.parse(result);
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            var weatherDesc = data.weather.main;
+            var weatherIcon = data.weather.icon;
+            var date = moment.unix(data.dt).format('DD/MM/YYYY');
+            $('#country').text(country + " ");
+            $('#generalDate').text(date);
+            $('#generalTemperature').text(data.main.temp);
+            $('#generalWind').text(data.wind.speed);
+            $('#generalHumidity').text(data.main.humidity);
+            $('#generalUvIndex').text('1');
 
-    // Axios({
-    //     method: 'get',
-    //     url: `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${APIKey}`,
-    //     headers : {}
-    // })
-    // .then((response) => {
-    //     console.log(JSON.stringify(response.data));
-    // })
-    // .catch((error) => {
-    //     console.log(error);
-    // });
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${APIKey}`).then(response => response.text())
-    .then(result => {
-        console.log(result);
-        var lat = result.lat;
-        var lon = result.lon;
-
-        // var date = moment.unix(result.date).format('MM/DD/YYYY');
-        // console.log(date);
+            fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`).then(response => response.text())
+            .then(result => {
 
 
-        // fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`).then(response => response.text())
-        // .then(result => {
-        //     console.log(result);
-        //     var lat = result.lat;
-        //     var lon = result.lon;
-            
-    
-        // })
-        // .catch(error => {
-        //     console.log('error: ', error)
-        // });
 
-    })
-    .catch(error => {
-        console.log('error: ', error)
-    });
+            })
+            .catch(error => {
+                console.log('error: ', error)
+            });
+
+        })
+        .catch(error => {
+            console.log('error: ', error)
+        });
 })
+
